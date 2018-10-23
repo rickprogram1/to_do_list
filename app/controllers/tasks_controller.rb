79b2dae9ac6a7d150c9539cbfa242e8a9c7d7e3c
rick.program1 <rick.program1@gmail.com>
2018-10-23 14:10:21 +0900

    Add tasks list

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,10 @@
 class TasksController < ApplicationController
-	before_action :logged_in_user, only: [:create, :destroy]
+	before_action :logged_in_user, only: [:index, :create, :destroy]
 	before_action :correct_user, only: :destroy
+
+	def index
+		@tasks = Task.paginate(page: params[:page])
+	end
 
 	def create
 		@task = current_user.tasks.build(task_params)
--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -6,6 +6,21 @@ class TasksControllerTest < ActionDispatch::IntegrationTest
     @task = tasks(:orange)
   end
 
+  test "should redirect index when not logged in" do
+    get tasks_path
+    assert_redirected_to login_url
+  end
+
+  test "index including pagination" do
+    log_in_as(users(:michael))
+    get tasks_path
+    assert_template 'tasks/index'
+    assert_select 'div.pagination'
+    Task.paginate(page: 1).each do |task|
+      assert_select 'a[href=?]', user_path(task.user), text: task.user.name
+    end 
+  end
+
   test "should redirect create when not logged in" do
     assert_no_difference 'Task.count' do
       post tasks_path, params: { task: { content: "Lorem ipsum" } }
